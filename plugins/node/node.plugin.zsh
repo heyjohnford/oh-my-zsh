666203cbe651bf262d8d4e3ffcf8c5652e3ffb05
johnamiahford <johnamiahford@gmail.com>
2013-01-26 15:32:38 -0500

    Merge node plugin with latest update

--- plugins/node/node.plugin.zsh
+++ plugins/node/node.plugin.zsh
@@ -1,9 +1,5 @@
 # Open the node api for your current version to the optional section.
 # TODO: Make the section part easier to use.
 function node-docs {
-<<<<<<< HEAD
-	open "http://nodejs.org/docs/latest/api/index.html"
-=======
 	open "http://nodejs.org/docs/$(node --version)/api/all.html#all_$1"
->>>>>>> 615e41b0ecdb25acba513fd09619bd56c2eb24eb
 }
